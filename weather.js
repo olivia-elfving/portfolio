a73ca7605241cfe2c26be49cfff9ff06cebf65b3
Olivia <olivia.elfving@hotmail.com>
2021-06-23 21:35:59 +0200

    weather api

---
+++ weather.js
@@ -0,0 +1,15 @@
+function init() {
+
+    fetch('https://secret-mountain-13843.herokuapp.com/gothenburg')
+        .then(response => response.json())
+        .then(function(data) {
+            let icon = data.weather[0].icon;
+            let kelvin = data.main.temp;
+            let celcius = `${Math.floor(kelvin - 273)}°C`;
+            
+            document.getElementById('weather_img').src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+            document.getElementById('weather').innerHTML = celcius;
+        });
+}
+window.addEventListener('DOMContentLoaded', init);
+
